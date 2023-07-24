a85dcc983239e9aa52522a76136c8f4e7633f1dd
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2023-07-24 12:07:09 -0500

    changes for service workers pwa

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -12,19 +12,19 @@ register(process.env.SERVICE_WORKER_FILE, {
   // registrationOptions: { scope: './' },
 
   ready(/* registration */) {
-    console.log('Service worker is active.')
+    console.log('Service worker is active.');
   },
   registered(/* registration */) {
     // console.log('Service worker has been registered.')
   },
   cached(/* registration */) {
-    console.log('Content has been cached for offline use.')
+    console.log('Content has been cached for offline use.');
   },
   updatefound(/* registration */) {
     // console.log('New content is downloading.')
   },
   updated(/* registration */) {
-    console.log('New content is available; Refresh...')
+    console.log('New content is available; Refresh...');
     setTimeout(() => {
       window.location.reload(true)
     }, 1000);
