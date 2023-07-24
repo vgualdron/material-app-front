f99742c873d11adff30f15fa8b2f62766b7d62ab
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2023-07-24 14:46:06 -0500

    change version 2.1

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -15,13 +15,13 @@ register(process.env.SERVICE_WORKER_FILE, {
     console.log('Service worker is active.');
   },
   registered(/* registration */) {
-    // console.log('Service worker has been registered.')
+    console.log('Service worker has been registered.');
   },
   cached(/* registration */) {
     console.log('Content has been cached for offline use.');
   },
   updatefound(/* registration */) {
-    // console.log('New content is downloading.')
+    console.log('New content is downloading.');
   },
   updated(/* registration */) {
     console.log('New content is available; Refresh...');
