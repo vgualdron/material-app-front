c926dae6eaa76a5cb6d5b01d87355fd069c2f987
victor.gualdron.r@gmail.com <vgualdron@estratec.local>
2023-07-27 15:50:38 -0500

    version 1.0.32

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,7 +26,7 @@
             class="no-shadow"
             push
             no-caps
-            label="Perfil"
+            label="Perfil -_-"
           >
             <div class="row no-wrap q-pa-md">
               <div class="column items-center">
